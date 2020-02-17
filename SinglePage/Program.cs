1e658080e1b0dbea1c9e00fbd1ca9d2a989c6a5f
Rich Bryant <rich.bryant@live.co.uk>
2020-02-17 14:51:38 +0000

    Add project files.

---
+++ SinglePage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace SinglePage
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddBlazorise(o => o.ChangeTextOnKeyPress = true)
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
+            builder.RootComponents.Add<App>("app");
+
+            var host = builder.Build();
+
+            host.Services.UseBootstrapProviders()
+                .UseFontAwesomeIcons();
+
+            await host.RunAsync();
+        }
+    }
+}
