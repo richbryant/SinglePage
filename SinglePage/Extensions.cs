4340128c5953e44726f6f831b1b9ba5c099ceca3
Rich Bryant <rich.bryant@live.co.uk>
2020-02-18 17:21:15 +0000

    Smooth scrolling fixed

---
+++ SinglePage/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace SinglePage
+{
+    public static class Extensions
+    {
+        public static ValueTask NavigateToFragmentAsync(this NavigationManager navigationManager, IJSRuntime jSRuntime)
+        {
+            var uri = navigationManager.ToAbsoluteUri(navigationManager.Uri);
+
+            return uri.Fragment.Length == 0 ? 
+                default : jSRuntime.InvokeVoidAsync("blazorHelpers.scrollToFragment", uri.Fragment.Substring(1));
+        }
+    }
+}
---
+++ SinglePage/FragmentNavigationLayoutBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
+
+namespace SinglePage
+{
+    public class FragmentNavigationLayoutBase : LayoutComponentBase, IDisposable
+    {
+        [Inject] NavigationManager NavManager { get; set; }
+        [Inject] IJSRuntime JsRuntime { get; set; }
+
+        protected override void OnInitialized()
+        {
+            NavManager.LocationChanged += TryFragmentNavigation;
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await NavManager.NavigateToFragmentAsync(JsRuntime);
+        }
+
+        private async void TryFragmentNavigation(object sender, LocationChangedEventArgs args)
+        {
+            await NavManager.NavigateToFragmentAsync(JsRuntime);
+        }
+
+        public void Dispose()
+        {
+            NavManager.LocationChanged -= TryFragmentNavigation;
+        }
+    }
+}
